db39b83d6ced3229c8b2c811f08eaa3d4bf4f5c9
2mastang1 <74460555+2mastang1@users.noreply.github.com>
2022-05-25 14:18:28 +0200

    la til kommentarer

--- src/app/treninger-info/treninger-info.component.ts
+++ src/app/treninger-info/treninger-info.component.ts
@@ -1,8 +1,7 @@
 import { Component, Inject, OnInit,  } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Data } from '@angular/router';
 import { TreningerComponent } from '../treninger/treninger.component';
-
-
 
 
 @Component({
@@ -14,6 +13,7 @@ import { TreningerComponent } from '../treninger/treninger.component';
 
 export class TreningerInfoComponent implements OnInit {
 
+//importerer listen med objekt fra treninger-component
   constructor(@Inject(MAT_DIALOG_DATA) public data: TreningerComponent["trening"]) { }
 
   ngOnInit(): void {
--- src/app/treninger/treninger.component.ts
+++ src/app/treninger/treninger.component.ts
@@ -10,13 +10,13 @@ import { TreningerInfoComponent } from '../treninger-info/treninger-info.compone
 })
 
 export class TreningerComponent {
-
+//her er listen over objekt som jeg bruker til å iterere over
   trening = [
     {id: 1, hvor: 'Træffhuset', info:'hello verden', img:'https://www.molde.kommune.no/handlers/bv.ashx/i2b07b822-66fe-44fc-b8c2-489ca253fedf/w1920/h1080/q1322/k14ab8e58db7a/20210716_135536.jpg'},
     {id: 2, hvor: 'Eidsvåg kunstgressbane', info:'hello verden 2', img:'https://www.molde.kommune.no/handlers/bv.ashx/i2304f203-d115-45f4-85ce-19db726db8ec/w1920/h1080/q240/k84aa8d327645/kunstgrasbanen-21-09-206-3.jpg'},
     {id: 3, hvor: 'Orkdal', info:'hello verden 3', img:'https://www.orkdal-il.no/assets/Album/g11-molde-cup-26-september-2015/IMG-8731.jpg'},
   ];
-
+//her er enda en variabel med en verdi av en komponent som brukes til å definere hva som skal åpnes i dialog boksen
   constructor(private dialog: MatDialog) { }
 
 openDialog() {
